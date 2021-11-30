21075feed8087c3156aa7c8e12f27153fa0922fe
shomffl <showatanabe0226@gmail.com>
2021-11-30 19:44:39 +0900

    static_pathを追加

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 import time
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../frontend/build/", static_url_path="")
 
 @app.route('/home')
 def get_current_time():
