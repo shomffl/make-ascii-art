332dfe765c9022bc105721524d31917398b08194
shomffl <showatanabe0226@gmail.com>
2021-12-01 20:20:23 +0900

    アスキーアート作成のための処理を追加

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,25 +1,36 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
 
-export const Home = () =>{
+export const Home: React.FC = () => {
+  const navigate = useNavigate();
   const [text, setText] = useState("hello");
+  const onClickPage = () => {
+    navigate("./page_a");
+  };
+
   const onClickGetTime = () => {
     axios.get("/home").then((res) => {
       setText(res.data.time);
     });
   };
+
+  useEffect(() => {
+    axios.get("/create_folder").then((res) => {
+      console.log(res.data.message);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {text}
         <button onClick={onClickGetTime}>get</button>
         <h1>Home</h1>
         <Link to="/page_a">PageA</Link>
+        {text}
+        <button onClick={onClickPage}>push</button>
       </header>
     </div>
   );
-}
+};
--- frontend/src/PageA.tsx
+++ frontend/src/PageA.tsx
@@ -1,12 +1,57 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import Stack from "@mui/material/Stack";
+import axios from "axios";
+import logo from "./apple.png";
 
 export const PageA: React.FC = () => {
+  const [image, setImage] = useState(logo);
+  const onClickSubmit = (e: any) => {
+    const form = new FormData(e.target);
+    const file = form.get("file");
+    console.log(file);
+
+    e.preventDefault();
+    const Upload = () => {
+      axios
+        .post("/make", form, {
+          headers: { "content-type": "multipart/form-data" },
+        })
+        .then((res) => {
+          setImage(`${process.env.PUBLIC_URL}/static/images/ascii_images/${res.data.name}`);
+          console.log(res);
+        });
+    };
+    Upload();
+  };
+
   return (
-    <div>
-      <h1>PageA</h1>
-      <Link to="/">Home</Link>
+    <div className="App">
+      <header className="App-header">
+        <img src={image} style={{width: "500px"}}/>
+        <form onSubmit={onClickSubmit}>
+          <Stack direction="row" spacing={2}>
+            <label>
+              <input
+                accept="image/*"
+                type="file"
+                name="file"
+                style={{ display: "none" }}
+              />
+              <Button variant="contained" component="span">
+                Upload
+              </Button>
+            </label>
+            <label>
+              <Button variant="contained" type="submit">
+                <span>Send</span>
+                <SendIcon />
+              </Button>
+            </label>
+          </Stack>
+        </form>
+      </header>
     </div>
   );
 };
