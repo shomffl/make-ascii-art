95e946940c41c9821c4f52a8242b975d17cf561f
shomffl <showatanabe0226@gmail.com>
2021-12-02 00:15:41 +0900

    apiフォルダを削除

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import time
 from flask import Flask, request
 from werkzeug.utils import secure_filename
-from backend.api.convert_filename import Kakasi
-from backend.api.resize import ResizeImage
-from backend.api.make import MakeGrayFrame
+from backend.convert_filename import Kakasi
+from backend.resize import ResizeImage
+from backend.make import MakeGrayFrame
 import os
 
 app = Flask(__name__, static_folder="frontend/build/", static_url_path="")
