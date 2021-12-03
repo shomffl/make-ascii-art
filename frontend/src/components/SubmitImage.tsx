c12df052fdef25c73dcf43d26cf7ee6f54502ac7
shomffl <showatanabe0226@gmail.com>
2021-12-03 18:23:13 +0900

    画像アップロード時の処理を追加

--- frontend/src/components/SubmitImage.tsx
+++ frontend/src/components/SubmitImage.tsx
@@ -5,6 +5,9 @@ import Stack from "@mui/material/Stack";
 import axios from "axios";
 import { DownloadImage } from "./DownloadImage";
 import { Remake } from "./Remake";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 export const SubmitImage: React.FC<{
   setImage: Dispatch<SetStateAction<string>>;
@@ -12,8 +15,12 @@ export const SubmitImage: React.FC<{
   const { setImage } = props;
   const [downloadPath, setDownloadPath] = useState("");
   const [openDownload, setOpenDownload] = useState(false);
+  const [canClickSend, setCanClickSend] = useState(true);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
   const onClickSubmit = (e: any) => {
     const form = new FormData(e.target);
+    setOpenSnackbar(false);
 
     e.preventDefault();
     const Upload = () => {
@@ -29,6 +36,23 @@ export const SubmitImage: React.FC<{
     };
     Upload();
   };
+  const imageSelected = (e: any) => {
+    setCanClickSend(false);
+    setOpenSnackbar(true);
+  };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={(e) => setOpenSnackbar(false)}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
 
   return (
     <>
@@ -38,7 +62,12 @@ export const SubmitImage: React.FC<{
           <Remake />
         </Stack>
       ) : (
-        <form onSubmit={onClickSubmit}>
+        <form onSubmit={onClickSubmit} onChange={imageSelected}>
+          <Snackbar
+            open={openSnackbar}
+            message="image was selected!!"
+            action={action}
+          />
           <Stack direction="row" spacing={2}>
             <label>
               <input
@@ -52,7 +81,7 @@ export const SubmitImage: React.FC<{
               </Button>
             </label>
             <label>
-              <Button variant="contained" type="submit">
+              <Button variant="contained" type="submit" disabled={canClickSend}>
                 <span>Send</span>
                 <SendIcon />
               </Button>
