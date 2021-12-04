7fc76dc8ea2174b81ff939c1d4c329af82c55a15
shomffl <showatanabe0226@gmail.com>
2021-12-04 21:44:31 +0900

    トップページにアニメーションを追加

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./App.css";
+import Button from "@mui/material/Button";
+import styled, { keyframes } from "styled-components";
+import fugaku from "./fugaku.png";
+import fugaku_ascii from "./fugaku_ascii.png";
+import black from "./black.png";
 
 export const Home: React.FC = () => {
   const navigate = useNavigate();
   const onClickPage = () => {
     navigate("./make");
   };
-
 
   useEffect(() => {
     axios.get("/create_folder").then((res) => {
@@ -17,10 +20,114 @@ export const Home: React.FC = () => {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={onClickPage}>push</button>
-      </header>
+    <div>
+      <Background>
+        <BlackImage1 src={black} style={{ animationDelay: "1s" }} />
+        <BlackImage1 src={black} style={{ animationDelay: "1.1s" }} />
+        <BlackImage1 src={black} style={{ animationDelay: "1.2s" }} />
+        <BlackImage1 src={black} style={{ animationDelay: "1.3s" }} />
+        <BlackImage1 src={black} style={{ animationDelay: "1.4s" }} />
+        <BlackImage1 src={black} style={{ animationDelay: "1.5s" }} />
+        <BlackImage1 src={black} style={{ animationDelay: "1.6s" }} />
+        <BlackImage1 src={black} style={{ animationDelay: "1.7s" }} />
+        <BlackImage1 src={black} style={{ animationDelay: "1.8s" }} />
+        <BlackImage1 src={black} style={{ animationDelay: "1.9s" }} />
+        <BlackImage2 src={black} style={{ animationDelay: "1.9s" }} />
+        <BlackImage2 src={black} style={{ animationDelay: "1.8s" }} />
+        <BlackImage2 src={black} style={{ animationDelay: "1.7s" }} />
+        <BlackImage2 src={black} style={{ animationDelay: "1.6s" }} />
+        <BlackImage2 src={black} style={{ animationDelay: "1.5s" }} />
+        <BlackImage2 src={black} style={{ animationDelay: "1.4s" }} />
+        <BlackImage2 src={black} style={{ animationDelay: "1.3s" }} />
+        <BlackImage2 src={black} style={{ animationDelay: "1.2s" }} />
+        <BlackImage2 src={black} style={{ animationDelay: "1.1s" }} />
+        <BlackImage2 src={black} style={{ animationDelay: "1s" }} />
+
+        {/* <StyledButton onClick={onClickPage} style={{height: "50px"}}>push</StyledButton> */}
+      </Background>
     </div>
   );
 };
+
+const StyledButton = styled(Button)`
+  position: absolute;
+  bottom: 50vh;
+`;
+
+const SlideLeft = keyframes`
+
+  from {
+    transform: translateX(0%);
+  }
+  30% {
+        transform: translateX(100%);
+  }
+70% {
+        transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0%);
+  }
+
+`;
+
+const SlideRight = keyframes`
+
+  from {
+    transform: translateX(0%);
+  }
+  30% {
+        transform: translateX(-100%);
+  }
+70% {
+        transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0%);
+  }
+
+`;
+
+const BlackImage1 = styled.img`
+  animation-name: ${SlideLeft};
+  height: 5vh;
+  width: 100vw;
+  animation-duration: 4s;
+  animation-timing-function: ease-in;
+  animation-fill-mode: forwards;
+  vertical-align: bottom;
+  position: relative;
+  left: -100vw;
+`;
+const BlackImage2 = styled.img`
+  animation-name: ${SlideRight};
+  height: 5vh;
+  width: 100vw;
+  animation-duration: 4s;
+  animation-timing-function: ease-in;
+  animation-fill-mode: forwards;
+  vertical-align: bottom;
+  position: relative;
+  right: -100vw;
+`;
+
+const fadeIn = keyframes`
+  from {
+     background-image: url(${fugaku});
+  }
+  70%{
+      background-image: url(${fugaku});
+  }
+  to {
+  }
+`;
+
+const Background = styled.header`
+  animation-name: ${fadeIn};
+  animation-duration: 4s;
+  animation-timing-function: ease-in;
+  background-image: url(${fugaku_ascii});
+  height: 100vh;
+  background-size: cover;
+  overflow: hidden;
+`;
--- frontend/src/components/ImageDisplay.tsx
+++ frontend/src/components/ImageDisplay.tsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { SubmitImage } from "./SubmitImage";
 import logo from "../apple.png";
 
+
 export const ImageDisplay = () => {
   const [image, setImage] = useState(logo);
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <img src={image} style={{ width: "500px" }} />
         <SubmitImage setImage={setImage} />
 
